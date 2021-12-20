6344a3ca3b8d1a67c576b82e2b43c7f93459d98e
zrellib <zrellib@MacBook-Pro-khxng-zrellib.local>
2021-12-20 17:13:08 +0700

    piscine03

---
+++ piscineC03/ex04/ft_strstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: npatthan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 16:06:59 by npatthan          #+#    #+#             */
+/*   Updated: 2021/11/03 16:09:00 by npatthan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdbool.h>
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	char	*haystack;
+	char	*needle;
+
+	if (*to_find == '\0')
+		return (str);
+	haystack = str;
+	needle = to_find;
+	while (true)
+	{
+		if (*needle == '\0')
+			return ((char *)(haystack - (needle - to_find)));
+		if (*haystack == *needle)
+			needle++;
+		else
+			needle = to_find;
+		if (*haystack == '\0')
+			break ;
+		haystack++;
+	}
+	return (0);
+}
